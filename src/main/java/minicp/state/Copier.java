a9e09c1a81349f8664a32bb16d4ed2162d654002
Laurent Michel <ldm@engr.uconn.edu>
2018-08-21 16:51:04 -0400

    done with copier

--- src/main/java/minicp/state/Copier.java
+++ src/main/java/minicp/state/Copier.java
@@ -1,53 +1,80 @@
 package minicp.state;
 
+import minicp.util.Procedure;
 import java.util.Stack;
 
-public class Copier extends AbstractStateManager {
+public class Copier implements StateManager {
 
     private Stack<Storage> store;
-
-    private StateEntry popStore = new StateEntry() {
-        @Override
-        public void restore() {
-            store.pop();
-        }
-    };
+    private Stack<Stack<StateEntry>> prior;
+    private Stack<StateEntry> backup;
 
     public Copier() {
-        store = new Stack<>();
+        store = new Stack<Storage>();
+        prior = new Stack<Stack<StateEntry>>();
+        backup = new Stack<StateEntry>();
+    }
+    public int getLevel()  { return prior.size()-1;}
+    public int storeSize() { return store.size();}
+
+    public void save() {
+        for (Storage s : store)
+            backup.add(s.save());
+        prior.add(backup);
+        backup = prepare();
+    }
+    private Stack<StateEntry> prepare() {
+        Stack<StateEntry> backup = new Stack<>();
+        final int curSize = store.size();
+        backup.add(new StateEntry() {
+            public void restore() { store.setSize(curSize);}
+        });
+        return backup;
+    }
+    public void restore() {
+        Stack<StateEntry> toRestore = prior.pop();
+        for(StateEntry se : toRestore)
+            se.restore();
+        backup = prepare();
     }
 
-    @Override public void save() {
-        super.save();
-        for (Storage s : store) {
-            pushState(s.save());
-        }
+    public void withNewState(Procedure body) {
+        final int level = getLevel();
+        save();
+        body.call();
+        restoreUntil(level);
     }
 
-    public int storeSize() {
-        return store.size();
-    };
+    /**
+     *  Restores the state as it was at level 0 (first save)
+     *  The level is now -1.
+     *  Notice that you'll probably want to save after this operation.
+     */
+    public void restoreAll() {
+        while (!prior.isEmpty())
+            restore();
+    }
+
+    public void restoreUntil(int level) {
+        while (getLevel() > level)
+            restore();
+    }
 
     public StateInt makeStateInt(int initValue) {
         CopyInt s = new CopyInt(initValue);
         store.add(s);
-        pushState(popStore);
         return s;
     }
 
     public StateBool makeStateBool(boolean initValue) {
         CopyBool s = new CopyBool(initValue);
         store.add(s);
-        pushState(popStore);
         return s;
     }
 
     public StateMap makeStateMap() {
         CopyMap s = new CopyMap<>();
         store.add(s);
-        pushState(popStore);
         return s;
     }
-
-
 }
